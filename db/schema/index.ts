a851195cffb26fd4ac0e6fe766fe3e6e0bcf1263
Essential Randomness <essential.randomn3ss@gmail.com>
2023-12-28 17:09:33 -0800

    database is working

---
+++ db/schema/index.ts
@@ -0,0 +1,50 @@
+import {
+  integer,
+  bigint,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  unique,
+} from "drizzle-orm/pg-core";
+
+export const characters = pgTable("characters", {
+  id: serial("id").primaryKey(),
+  ao3_id: bigint("ao3_id", {
+    mode: "number",
+  })
+    .notNull()
+    .unique(),
+  name: text("name").notNull(),
+  canonical_id: serial("canonical_id").references(() => characters.id),
+});
+
+export const certificates = pgTable("certificates", {
+  id: serial("id").primaryKey(),
+  hash: text("hash").notNull().unique(),
+  image_url: text("image_url").notNull(),
+});
+
+export const certificate_characters = pgTable(
+  "certificate_characters",
+  {
+    id: serial("id").primaryKey(),
+    certificate_id: serial("certificate_id").references(() => certificates.id),
+    character_id: serial("character_id").references(() => characters.id),
+    position: integer("position").notNull(),
+    image_url: text("image_url").notNull(),
+  },
+  (table) => ({
+    unique_certificate_position: unique("unique_certificate_position").on(
+      table.certificate_id,
+      table.position
+    ),
+  })
+);
+
+export const purchases = pgTable("purchases", {
+  id: serial("id").primaryKey(),
+  certificate_id: serial("certificate_id").references(() => certificates.id),
+  stripe_id: text("stripe_id"),
+  paypal_id: text("paypal_id"),
+});
